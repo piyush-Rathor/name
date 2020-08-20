0a53ec535105c28796b150ee4cd50b4010d1163b
piyush-Rathor <psrathor16072000@gmail.com>
2020-08-20 03:07:20 -0700

    finish Cookie and Session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
+const user = require("../models/user");
+
 exports.getLogin=(req,res,next)=>{
-    console.log(req.session.isLoggedIn);
     res.render('auth/login',{
         path:'/login',
         pageTitle:'Login',
@@ -8,8 +9,19 @@ exports.getLogin=(req,res,next)=>{
 };
 
 exports.postLogin=(req,res,next)=>{
-    req.session.isLoggedIn=true;
+    user.findById('5f2c3d95e8e0171016209c61')
+    .then(user=>{
+        req.session.isLoggedIn=true;
+        req.session.user=user;
     res.redirect('/');
+    })
+    .catch(err=>console.log(err));
 };
+
+exports.postLogout=(req,res,next)=>{
+    req.session.destroy((err)=>{
+        res.redirect('/');
+    })
+}
 //here this session is saved in memory and memory is limited or u thougents or one hundred thoagent requests so u can not manage your memory 
 //so now v r going to save session in database
